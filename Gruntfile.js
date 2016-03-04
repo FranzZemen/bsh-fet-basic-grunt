1301b2047e4109476750ce88250ba8e2976b530b
FranzZemen <FranzZemen@gmail.com>
2016-03-04 07:48:34 -0600

    grunt 103 video snippet

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,11 +19,30 @@ module.exports = function (grunt) {
                     // https://github.com/taptapship/wiredep#configuration
                 }
             }
+        },
+        copy: {
+            dev: {
+                files: [
+                    {src: 'index.html', dest: 'dev/'},
+                    {src: ['templates/**', 'js/**'], dest:'dev/'}
+                ]
+            },
+            dist: {
+                files: [
+                    {src: 'index.html', dest: 'dist/'},
+                    {src: ['templates/**', 'js/**'], dest:'dist/'}
+                ]
+            }
+        },
+        clean: {
+            dev: ['./dev'],
+            dist: ['./dist']
         }
     });
     // Load npm plugin tasks
     grunt.loadNpmTasks('grunt-wiredep');
-
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     // define task
     grunt.registerTask('echo', function () {
         grunt.log.write('Hello World');
